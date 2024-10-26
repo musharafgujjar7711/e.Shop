1c830bc2559dd427ac0dc6e595e66457871b1fb4
Your_Name <musharafgujjar7711@gmail.com>
2024-10-26 08:01:35 +0500

    done

--- src/Coponents/Category.jsx
+++ src/Coponents/Category.jsx
@@ -1,47 +1,49 @@
-import React from 'react'
+import React from 'react';
+import WomanCategory from '../assets/Images/WomanCart.png'
+const categoryData = [
+  {
+    id: 1,
+    title: 'Kids',
+    bgColor: 'bg-blue-200',
+    image: 'public/imges/man.png',
+  },
+  {
+    id: 2,
+    title: 'Women',
+    bgColor: 'bg-yellow-200',
+    image: WomanCategory,
+  },
+  {
+    id: 3,
+    title: 'Men',
+    bgColor: 'bg-red-200',
+    image: 'public/imges/kids.png',
+  },
+];
 
 function Category() {
   return (
-    <div className=' bg-white pb-6 pt-8'>
-        <div className=' container mx-auto  '>
-            <div className=' flex items-center gap-3 flex-col md:flex-row'>
-
-            
-            <div className=' w-96 h-72 flex items-center border bg-blue-200 px-6 rounded text-center relative'>
-              <h1 className='w-24'>
-               <b> kids</b> 
-              <div className=''> view All</div>
+    <div className="bg-white pb-6 pt-8">
+      <div className="container mx-auto">
+        <div className="flex items-center gap-3 flex-col md:flex-row">
+          {categoryData.map((category) => (
+            <div
+              key={category.id}
+              className={`w-96 h-72 flex items-center border ${category.bgColor} px-6 rounded text-center relative`}
+            >
+              <h1 className="w-36">
+                <b>{category.title}</b>
+                <div className="">View All</div>
               </h1>
-            <div className=' '>
-            <img src="public/imges/man.png" alt=""  className='w-80 h-72 ' />
+              <div className="">
+                <img src={category.image} alt={category.title} className="w-80 h-72" />
+              </div>
             </div>
-            </div>
-
-            <div className=' w-96 h-72 flex items-center border bg-yellow-200 px-6 rounded text-center relative'>
-              <h1 className='w-36'>
-               <b> kids</b> 
-              <div className=''> view All</div>
-              </h1>
-            <div className=' '>
-            <img src="public/imges/woman.png" alt=""  className='w-80 h-72 ' />
-            </div>
-            </div>
-           
-            <div className=' w-96 h-72 flex items-center border bg-red-200 px-2 rounded text-center'>
-              <h1 className=' flex-1 '>
-               <b> kids</b> 
-              <div className=''> view All</div>
-              </h1>
-            <div className=' flex-1'>
-            <img src="public/imges/kids.png" alt=""  className='w-80 h-72 ' />
-            </div>
-            </div>
-            
-
-            </div>
+          ))}
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Category
+export default Category;
